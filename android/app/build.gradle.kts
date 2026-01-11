0b1d1838755eddc6c24a9be427c7aa32eba75cdd
dhruvgoel3 <dhruvgoel1100@gmail.com>
2026-01-12 00:41:11 +0530

    DriveResQ App Module 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,16 +18,20 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
+
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.driveresq_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        flutter.minSdkVersion 21
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
